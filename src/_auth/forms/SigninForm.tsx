86158ffa7a2b9054db083480cd4f27a08c7fecea
Fezaan <110244796+Fezaan@users.noreply.github.com>
2024-01-14 19:34:30 +0530

    Removed Bugs

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -9,7 +9,7 @@ import { SigninValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
 import { Link, useNavigate } from "react-router-dom";
 // import { createUserAccount } from "@/lib/appwrite/api";
-import { toast, useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 import { useSignInAccount } from "@/lib/react-query/queriesAndMutations";
 import { useUserContext } from "@/context/AuthContext";
  
